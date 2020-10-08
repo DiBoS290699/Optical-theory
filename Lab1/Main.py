796eeda2e73a3d2972ef23d42f4452a5eaff72ff
DiBoS290699 <dimaborisov290699@yandex.ru>
2020-10-09 00:24:53 +0400

    Surfaces is added

--- Lab1/Main.py
+++ Lab1/Main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.rc('axes.formatter', useoffset=False)
 
 
 def func(x):
@@ -12,9 +14,7 @@ def kernel(x, eps, alpha=1.0):
 
 def Fs(xs, epss, alpha=1.0):
     h_x = xs[1] - xs[0]
-    matrix_A = []
-    for eps in epss:
-        matrix_A.append([kernel(xs, eps, alpha)])
+    matrix_A = kernel(xs[None, :], epss[:, None], alpha)
     vector_f = func(xs)
     Fs = np.dot(matrix_A, np.transpose(vector_f)) * h_x
     return Fs
@@ -34,26 +34,31 @@ def plots_f(xs):
 
 def plots_F(xs, epss, alpha=1.0):
     F = Fs(xs, epss, alpha)
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-    axes[0].set_title('График модуля результата преобразования')
+    fig, axes = plt.subplots(1, 2, figsize=(13, 8))
+    axes[0].set_title('График амплитуды выходного сигнала')
     axes[0].plot(xs, np.abs(F))
     axes[0].grid()
-    axes[1].set_title('График аргумента результата преобразования')
+
+    axes[1].set_title('График фазы выходного сигнала')
     axes[1].plot(xs, np.angle(F))
     axes[1].grid()
     plt.show()
 
 
-# def plots_K(xs, epss, alpha=1):
-#     K = kernel(xs, epss, alpha)
-#     fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-#     axes[0].set_title('График модуля ядра')
-#     axes[0].plot(epss, np.abs(K))
-#     axes[0].grid()
-#     axes[1].set_title('График аргумента ядра')
-#     axes[1].plot(epss, np.angle(K))
-#     axes[1].grid()
-#     plt.show()
+def plots_K(xs, epss, alpha=1.0):
+    X, Y = np.meshgrid(xs, epss)
+    K = kernel(X, Y, alpha)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, np.abs(K))
+    plt.title('График амплитуды ядра')
+    plt.show()
+    plt.close(fig)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, np.angle(K))
+    plt.title('График фазы ядра')
+    plt.show()
 
 
 a = -200
@@ -62,13 +67,9 @@ n = 1000
 p = -2
 q = 2
 m = 1000
-alpha = 0.5
+alpha = 1
 xs = np.linspace(a, b, n)
-h_x = xs[1] - xs[0]
 epss = np.linspace(p, q, m)
-h_eps = epss[1] - epss[0]
 plots_f(xs)
 plots_F(xs, epss, alpha)
-
-
-
+plots_K(xs, epss, alpha)
